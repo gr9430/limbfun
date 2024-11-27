354894727fe5c5ddb8148cf74c8211f90a61e260
GR <glennritchey@proton.me>
2024-11-27 13:59:45 -0500

    Update waves.js

--- enhancedprojects/wavesgame/waves.js
+++ enhancedprojects/wavesgame/waves.js
@@ -5,7 +5,7 @@ const totalInteractions = 5; // Number of interactive zones
 let revealedMessages = [];
 
 function preload() {
-    img = loadImage('https://gr9430.github.io/ENG6806/enhancedprojects/wavesgame/images/canvas.jpg',
+    img = loadImage('https://gr9430.github.io/ENG6806/enhancedprojects/wavesgame/images/canvas.jpg');
         () => console.log('Image loaded successfully'),
         () => console.error('Failed to load image'));
 }
