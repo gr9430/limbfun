c7781aa59372adb43238963fd0b2cd663a5ea98e
GR <glennritchey@proton.me>
2024-12-01 20:44:54 -0500

    games

--- enhancedprojects/wavesgame/waves.js
+++ enhancedprojects/wavesgame/waves.js
@@ -10,7 +10,7 @@ function preload() {
         () => console.error('Failed to load image')
     );
 }
-
+  
 function setup() {
     console.log('Starting setup...');
     let canvasContainer = document.getElementById('canvas-container');
